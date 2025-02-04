ecccbab0b6d845f3c7975adb7e921e516c198719
Obinna Akpulonu <akpulonujoseph@gmail.com>
2025-02-04 13:36:40 +0100

    Update utils.py

--- backend/api/utils.py
+++ backend/api/utils.py
@@ -1,12 +1,9 @@
-
-
-
 def chat_with_ai(message):
     try:
         from gradio_client import Client
         client = Client("sakthisurya/textbasedAI")
         result = client.predict(
-            message,	# str in 'Message' Textbox component
+            message,
             api_name="/chat"
         )
         print("hey Chief here's the Gradio API result:", result)
